b68fa37eddfc4cab6dcba007cbb4ac0a9f2531d4
Yogesh Sanjay Birajdar <yogeshbirajdar030@gmail.com>
2025-11-19 17:29:20 +0530

    Refactor Adidas Sales Dashboard code structure

--- Adidas_Sales.py
+++ Adidas_Sales.py
@@ -368,4 +368,3 @@ with dwn5:
     st.download_button("Get Raw Data", data = filtered_df.to_csv().encode("utf-8"),
                        file_name = "SalesRawData.csv", mime="text/csv")
     
-st.divider()
